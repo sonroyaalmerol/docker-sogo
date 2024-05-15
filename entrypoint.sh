7bababa5646e8cd024fccf1303d5452cbbbd0dd9
Son <github@snry.me>
2024-05-15 15:26:52 -0400

    supervisord: run sogod.sh as root then step down to sogo user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-if [ -z "$LD_PRELOAD" ]; then
-  LIBSSL_LOCATION=$(find / -type f -name "libssl.so.*" -print -quit 2>/dev/null)
-  echo "LD_PRELOAD=$LIBSSL_LOCATION" >> /etc/default/sogo
-  echo "LD_LIBRARY_PATH=/usr/local/lib/sogo:$LD_LIBRARY_PATH" >> /etc/default/sogo
-  export LD_PRELOAD=$LIBSSL_LOCATION
-else
-  echo "LD_PRELOAD=$LD_PRELOAD" >> /etc/default/sogo
-  if [ -z "$LD_LIBRARY_PATH" ]; then
-    echo "LD_LIBRARY_PATH=/usr/local/lib/sogo:/usr/local/lib:/usr/lib" >> /etc/default/sogo
-  else
-    echo "LD_LIBRARY_PATH=/usr/local/lib/sogo:/usr/local/lib:$LD_LIBRARY_PATH" >> /etc/default/sogo
-  fi
-  export LD_PRELOAD=$LD_PRELOAD
-fi
-
 # Set process UID and GID at runtime
 if [ -n "$PUID" ] && [ -n "$PGID" ]; then
   groupmod -g $PGID sogo
